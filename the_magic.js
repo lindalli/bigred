618ceb8302ac308c36a8e72540b01966e4ab4064
Cristhian Ulloa <ulloac@mit.edu>
2014-09-27 09:42:53 -0400

    added images to buttons. Created back end file

--- the_magic.js
+++ the_magic.js
@@ -3,16 +3,16 @@
 //defining global contant variables
 var NUM_BUTTONS = 9;
 var FOOD = "food";
-var MUSIC = "music";
+var MUSIC = "concerts";
 var NIGHTLIFE = "nightlife";
 var MOVIES = "movies";
-var STAY = "stay";
-var SHOP = "shop";
+var STAY = "stayingin";
+var SHOP = "shopping";
 var ACTIVE = "active";
 var ATTRACTIONS = "attractions";
 var SPECIAL = "special";
 
-var BUTTON_LIST = [NUM_BUTTONS, FOOD, MUSIC, NIGHTLIFE, MOVIES, STAY, SHOP, ACTIVE, ATTRACTIONS, SPECIAL];
+var BUTTON_LIST = [FOOD, MUSIC, NIGHTLIFE, MOVIES, STAY, SHOP, ACTIVE, ATTRACTIONS, SPECIAL];
 
 Array.prototype.foreach = function(f) {
     for (var i = 0; i < this.length; ++i) {
@@ -26,10 +26,52 @@ var makeHomeButtons = function()
 	{
 		var button = document.createElement("BUTTON");
 		button.setAttribute("id", BUTTON_LIST[i]);
+		var img = document.createElement("IMG");
+		img.src = "data/" + BUTTON_LIST[i] + ".png";
 		button.setAttribute("class", "homebutton");
-		button.innerHTML = BUTTON_LIST[i];
+		button.appendChild(img);
 		document.getElementById("button div").appendChild(button);
 	}
 }
+
+$(document).ready(function() {
+    			$("button").click(function() { if($(this).attr('id') === FOOD)
+    			{
+    				console.log(FOOD);
+    			}
+    			else if($(this).attr('id') === MUSIC)
+    			{
+    				console.log(MUSIC);
+    			}
+				else if($(this).attr('id') === NIGHTLIFE)
+    			{
+    				console.log(NIGHTLIFE);
+    			}
+    			else if($(this).attr('id') === MOVIES)
+    			{
+    				console.log(MOVIES);
+    			}
+    			else if($(this).attr('id') === STAY)
+    			{
+    				console.log(STAY);
+    			}
+    			else if($(this).attr('id') === SHOP)
+    			{
+    				console.log(SHOP);
+    			}
+    			else if($(this).attr('id') === ACTIVE)
+    			{
+    				console.log(ACTIVE);
+    			}
+    			else if($(this).attr('id') === ATTRACTIONS)
+    			{
+    				console.log(ATTRACTIONS);
+    			}
+    			else if($(this).attr('id') === SPECIAL)
+    			{
+    				console.log(SPECIAL);
+    			}
+    			});
+    	});
 
 makeHomeButtons();
